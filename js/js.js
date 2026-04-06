21c1f0bd15aa611fbedc849bcf0b85123d3237a1
Álvaro Zurdo Álvarez <alvarozural@gmail.com>
2026-04-06 02:51:14 +0200

    mejoras y añadida la pagina de registro

---
+++ js/js.js
@@ -0,0 +1,77 @@
+
+async function registro(){
+    let username = document.getElementById('inputUser').value;
+    let email = document.getElementById('inputEmail').value;
+    let password = document.getElementById('inputPassword').value;
+    let response = await fetch('http://localhost:8080/api/v1/tfg', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            "nombre_usuario": username,
+            "email": email,
+            "contrasena": password,
+            "esLogin": false
+        })
+    })
+    const resultado = await response.json();
+    if(!response.ok){
+        document.querySelectorAll("[id$='_error']").forEach(el => {
+            el.textContent = "";
+            el.style.display = 'none'
+        });
+        resultado.forEach(error => {
+            console.error(`Campo: ${error.field} - ${error.defaultMessage}`);
+
+            const elemento = document.getElementById(error.field + "_error");
+            if (elemento) { // ← comprueba que existe antes de asignar
+                elemento.textContent = error.defaultMessage;
+                elemento.style.display = 'block';
+            }
+        });
+    } else {
+        cookieStore.set('username', username);
+        cookieStore.set('email', email);
+        cookieStore.set('password', password);
+    }
+}
+
+
+async function login(){
+    let username = document.getElementById('inputUser').value;
+    let password = document.getElementById('inputPassword').value;
+    let response = await fetch('http://localhost:8080/api/v1/tfg/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            "nombre_usuario": username,
+            "contrasena": password,
+            "esLogin": true
+        })
+    })
+    const resultado = await response.json();
+    if(!response.ok){
+        document.querySelectorAll("[id$='_error']").forEach(el => {
+            el.textContent = "";
+            el.style.display = 'none'
+        });
+        resultado.forEach(error => {
+            console.error(`Campo: ${error.field} - ${error.defaultMessage}`);
+
+            const elemento = document.getElementById(error.field + "_error");
+            if (elemento) { // ← comprueba que existe antes de asignar
+                elemento.textContent = error.defaultMessage;
+                elemento.style.display = 'block';
+            }
+        });
+    } else {
+        cookieStore.set('username', response.nombre_usuario);
+        cookieStore.set('email', response.email);
+        cookieStore.set('password', response.contrasena);
+    }
+}
